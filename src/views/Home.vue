5573deed922c0616b8e05632b64330e978add8ab
Vinicius Bazanella <v.bazanella@edu.pucrs.br>
2022-03-14 02:25:32 +0000

    Create boilerplate

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <Welcome />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Welcome from "../components/Welcome.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: { Welcome },
+});
+</script>
