c2a2922266e2fa6f2c5f680f3aa49a04c7ab11d7
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2022-03-25 22:06:23 -0300

    page-login | ajusta tela nas proporcoes do mock e muda fonte de titulo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="container">
-    <div class="title mt-5">Adote um Amanhã</div>
-    <div class="img-container">
-      <img src="../assets/img/logo.svg" alt="" />
-    </div>
-    <div class="title mt-6">Bem-vindo!</div>
-    <div class="inputs mt-6">
-      <v-text-field class="mx-10" outlined label="E-mail"></v-text-field>
-      <PasswordInput class="mx-10" />
-      <div class="d-flex justify-center">
-        <v-btn elevation="1" rounded class="px-10">Entrar</v-btn>
+  <v-container fill-height class="align-start">
+    <v-container class="align-start">
+      <div class="a-title">Adote um Amanhã</div>
+      <div class="img-container">
+        <img src="../assets/img/logo.svg" alt="" />
       </div>
-    </div>
+      <div class="a-title mt-6">Bem-vindo!</div>
+    </v-container>
+    <v-container class="align-center">
+      <div class="inputs mt-6">
+        <v-text-field class="mx-10" outlined label="E-mail"></v-text-field>
+        <PasswordInput class="mx-10" />
+        <div class="d-flex justify-center">
+          <v-btn elevation="1" rounded class="px-10">Entrar</v-btn>
+        </div>
+      </div>
+    </v-container>
 
-    <div class="footer-texts">
-      <v-row>
-        <span>Esqueceu sua senha?</span>
-      </v-row>
-      <v-row>
-        <span>Esqueceu sua senha?</span>
-      </v-row>
-    </div>
-  </div>
+    <!-- <v-spacer /> -->
+    <v-container class="align-end">
+      <v-col class="mt-10" align-self="stretch">
+        <v-row class="d-flex justify-center">
+          <span>Ainda não tem conta?</span>&nbsp;<a>Cadastre-se!</a>
+        </v-row>
+      </v-col>
+    </v-container>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -36,18 +40,20 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-.title {
-  font-size: 24px;
-  font-weight: 400;
-  color: #191919;
-  text-align: center;
-}
+.v-application {
+  .a-title {
+    font-size: 26px;
+    font-weight: 400;
+    color: #191919;
+    text-align: center;
+  }
 
-.img {
-  &-container {
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  .img {
+    &-container {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
   }
 }
 </style>
