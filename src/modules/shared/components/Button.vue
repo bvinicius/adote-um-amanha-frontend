cab16968e90a80403df8781f19bfd2d2483dd694
hcley <cleysonb.oliveira@gmail.com>
2022-03-26 19:03:13 -0300

    Adicionado shared component para embedded video
    Concluido page necessidades da instituição
    
    padronizado inclusão de icones no shared componente button

--- src/modules/shared/components/Button.vue
+++ src/modules/shared/components/Button.vue
@@ -1,11 +1,14 @@
 <template>
   <v-btn
-    class="a-button px-9 py-5"
+    class="a-button v-application px-9 py-5"
     elevation="0"
     v-bind="$attrs"
     v-on="$listeners"
-    >{{ title }}</v-btn
   >
+    <v-icon v-if="prependIcon" left dark>{{ prependIcon }}</v-icon>
+    {{ title }}
+    <v-icon v-if="appendIcon" right dark>{{ appendIcon }}</v-icon>
+  </v-btn>
 </template>
 
 <script lang="ts">
@@ -13,6 +16,8 @@ import Vue from "vue";
 export default Vue.extend({
   props: {
     title: String,
+    prependIcon: String,
+    appendIcon: String,
   },
 });
 </script>
@@ -24,5 +29,14 @@ export default Vue.extend({
   color: #191919 !important;
   font-size: 16px !important;
   font-weight: 400 !important;
+  border: 2px solid #ffd25a !important;
+}
+
+.v-btn--outlined {
+  color: #191919 !important;
+}
+
+.v-application .primary--text {
+  color: #191919 !important;
 }
 </style>
