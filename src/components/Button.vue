acab293da96fc6a967a603ee8ed9af87f8ac3491
Vinicius Bazanella <v.bazanella@edu.pucrs.br>
2022-03-26 16:50:35 +0000

    Padroniza estilização

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn
+    class="a-button px-9 py-5"
+    elevation="0"
+    v-bind="$attrs"
+    v-on="$listeners"
+    >{{ title }}</v-btn
+  >
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    title: String,
+  },
+});
+</script>
+
+<style scoped>
+.a-button {
+  text-transform: none !important;
+  border-radius: 16px !important;
+  color: #191919 !important;
+  font-size: 16px !important;
+  font-weight: 400 !important;
+}
+</style>
---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <Input
+    v-on="$listeners"
+    v-bind="$attrs"
+    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+    :type="showPassword ? 'text' : 'password'"
+    @click:append="onIconClick"
+  />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Input from "./Input.vue";
+
+export default Vue.extend({
+  inheritAttrs: false,
+  components: {
+    Input,
+  },
+  data: () => ({
+    showPassword: false,
+  }),
+  methods: {
+    onIconClick() {
+      this.showPassword = !this.showPassword;
+    },
+  },
+});
+</script>
+
+<style></style>
