8a442020965fd43582caa4710b1288c2f4efe16d
bvinicius <vinicius.bazanella1@gmail.com>
2022-03-30 19:02:08 -0300

    WIP - inicia a edição da necessidade

---
+++ src/modules/institution/views/EditNecessity.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container class="px-8">
+    <v-row class="mt-8">
+      <Input label="Assunto" v-model="necessity.title" />
+    </v-row>
+    <v-row>
+      <Select
+        :items="testItems"
+        placeholder="Subcategoria"
+        v-model="necessity.subcategory"
+      />
+    </v-row>
+    <v-row>
+      <TextArea
+        outlined
+        name="input-descricao"
+        label="Descrição"
+        value="Descrição da necessidade."
+        v-model="necessity.description"
+      ></TextArea>
+    </v-row>
+    <v-row>
+      <v-radio-group v-model="necessity.category">
+        <v-radio :label="`Radio ${n}`" :value="n - 1"></v-radio>
+      </v-radio-group>
+    </v-row>
+    <v-row class="justify-center">
+      <Button title="Excluir" class="mr-6" />
+      <Button title="Salvar" />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Vue from "vue";
+import Input from "../../shared/components/Input.vue";
+import Button from "../../shared/components/Button.vue";
+import Select from "../../shared/components/Select.vue";
+import TextArea from "../../shared/components/TextArea.vue";
+import Categories, { Category } from "../../shared/enums/Category";
+import SubCategories, { Subcategory } from "../../shared/enums/Subcategory";
+export default Vue.extend({
+  mounted() {
+    this.$root.showToolbar("NECESSIDADE");
+  },
+  components: {
+    Input,
+    Button,
+    Select,
+    TextArea,
+  },
+  data: () => ({
+    testItems: ["Valor1", "Valor2", "Valor3"],
+    necessity: {
+      id: 12,
+      title: "Necessidade x",
+      createdDate: new Date(),
+      category: Category.service,
+      subcategory: Subcategory.health,
+      url: "google.com",
+      description: "Teste descrição",
+    },
+  }),
+});
+Button;
+</script>
+
+<style></style>
---
+++ src/modules/shared/components/Select.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-select v-bind="$attrs" v-on="$listeners" outlined></v-select>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  inheritAttrs: false,
+});
+</script>
+
+<style lang="scss" scoped>
+.v-select {
+  border-radius: 10px;
+
+  &--outlined {
+    ::v-deep fieldset {
+      border-color: var(--v-primary-base);
+    }
+  }
+}
+</style>
