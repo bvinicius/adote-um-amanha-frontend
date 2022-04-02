1720c46d6a49933be7512559c98c0dcfe2e36ca7
Vinicius Bazanella <vbazanella@nelogica.com.br>
2022-04-02 17:26:56 -0300

    Adiciona documentação e testes no fluxo de categoria

--- src/modules/shared/enums/Category.ts
+++ src/modules/shared/enums/Category.ts
@@ -5,26 +5,54 @@ export enum Category {
   service,
 }
 
+/**
+ * Retorna um texto no plural que representa o enum de categoria passado por parâmetro.
+ * @param category
+ * @returns {string}
+ */
 function toPluralString(category: Category): string {
   return pluralCategoryStrings[category];
 }
 
+/**
+ * Retorna um texto no singular que representa o enum de categoria passado por parâmetro.
+ * @param category
+ * @returns {string}
+ */
 function toSingularString(category: Category): string {
   return singularCategoryStrings[category];
 }
 
+/**
+ * Retorna uma lista com objetos do tipo `EnumerableObject`, um para cada valor possível do enum. O campo `name` será retornado no plural.
+ * @returns {EnumerableObject[]}
+ */
 function allPluralObjects(): EnumerableObject[] {
   return allObjects(pluralCategoryStrings);
 }
 
+/**
+ * Retorna uma lista com objetos do tipo `EnumerableObject`, um para cada valor possível do enum. O campo `name` será retornado no singular.
+ * @returns {EnumerableObject[]}
+ */
 function allSingularObjects(): EnumerableObject[] {
   return allObjects(singularCategoryStrings);
 }
 
+/**
+ * Retorna um objeto na estrutura do `EnumerableObject` para o enum passado por parâmetro. O campo `name` será retornado no plural.
+ * @param category
+ * @returns {EnumerableObject}
+ */
 function toPluralObject(category: Category): EnumerableObject {
   return toObject(category, pluralCategoryStrings);
 }
 
+/**
+ * Retorna um objeto na estrutura do `EnumerableObject` para o enum passado por parâmetro. O campo `name` será retornado no singular.
+ * @param category
+ * @returns {EnumerableObject}
+ */
 function toSingularObject(category: Category): EnumerableObject {
   return toObject(category, singularCategoryStrings);
 }
@@ -34,7 +62,7 @@ function toObject(
   categoryMap: CategoryMap
 ): EnumerableObject {
   return {
-    value: category,
+    value: category.toString(),
     name: categoryMap[category],
   };
 }
---
+++ tests/unit/categories/subcategory.spec.ts
@@ -0,0 +1,59 @@
+import SubcategoryUtils, {
+  Subcategory,
+} from "@/modules/shared/enums/Subcategory";
+
+describe("SubcategoryUtils", () => {
+  describe("toString", () => {
+    const cases = [
+      [Subcategory.education, "Educação"],
+      [Subcategory.food, "Alimentação"],
+      [Subcategory.health, "Saúde"],
+      [Subcategory.professionalization, "Profissionalização"],
+      [Subcategory.others, "Outros"],
+    ];
+    test.each(cases)(
+      "given %p as argument, should return %p.",
+      (subcategoryKey, expectedString) => {
+        const result = SubcategoryUtils.toString(subcategoryKey as Subcategory);
+        expect(result).toBe(expectedString);
+      }
+    );
+  });
+
+  describe("toObject", () => {
+    const cases = [
+      [Subcategory.health, { name: "Saúde", value: "0" }],
+      [
+        Subcategory.professionalization,
+        { name: "Profissionalização", value: "1" },
+      ],
+      [Subcategory.education, { name: "Educação", value: "2" }],
+      [Subcategory.food, { name: "Alimentação", value: "3" }],
+      [Subcategory.others, { name: "Outros", value: "4" }],
+    ];
+    test.each(cases)(
+      "given %p as argument, should return %p.",
+      (subcategoryKey, expectedObject) => {
+        const result = SubcategoryUtils.toObject(subcategoryKey as Subcategory);
+        expect(result).toMatchObject(expectedObject);
+      }
+    );
+  });
+
+  describe("allObjects", () => {
+    it("should return a list containing all objects with plural strings", () => {
+      const result = SubcategoryUtils.allObjects();
+      const expected = [
+        { value: `${Subcategory.health}`, name: "Saúde" },
+        {
+          value: `${Subcategory.professionalization}`,
+          name: "Profissionalização",
+        },
+        { value: `${Subcategory.education}`, name: "Educação" },
+        { value: `${Subcategory.food}`, name: "Alimentação" },
+        { value: `${Subcategory.others}`, name: "Outros" },
+      ];
+      expect(result).toMatchObject(expected);
+    });
+  });
+});
